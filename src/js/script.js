1b26c0854c51156f2b5c108c0264623266ef2b0a
ArsenGitHub <96071969+ArsenGitHub@users.noreply.github.com>
2022-08-05 22:17:59 +0500

    fixed menu animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const burger = document.querySelector('.burger'),
         menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.nav__cross, .menu__overlay');
+        closeCross = document.querySelector('.menu__cross'),
+        closeOverlay = document.querySelector('.menu__overlay');
 
 burger.addEventListener('click', () => {
     menu.classList.add('menu--enabled');
 });
 
-closeElem.addEventListener('click', () => {
+closeCross.addEventListener('click', () => {
+    menu.classList.remove('menu--enabled');
+});
+
+closeOverlay.addEventListener('click', () => {
     menu.classList.remove('menu--enabled');
 });
